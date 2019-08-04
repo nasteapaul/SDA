73ef16cd0f7c7496b6d33d4180685c9a273e1e1f
root <root@localhost.localdomain>
2019-08-04 13:13:14 -0400

    Main

---
+++ Main.java
@@ -0,0 +1,38 @@
+package advCode;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	String a = "Hello World!";
+		//System.out.println("Hello World!");
+		//System.out.println(a);
+		//for(int i = 0; i < 2 ; i++)
+			//System.out.println("Hello World!");
+		
+		
+	//	Scanner x = new Scanner(System.in);
+		//Scanner y = new Scanner(System.in);
+		//System.out.println("Enter a double number: ");
+		//double decNum = x.nextDouble();
+		//System.out.println("Enter how many decimals: ");
+		//int dec = y.nextInt();
+		//if(dec >0) {
+		//System.out.printf("%." + dec + "f", decNum);
+		//}else System.out.println("Error!");
+		
+	//	System.out.printf("%15s", "Test1");
+	//	System.out.printf("%15d", 2);
+	//	System.out.printf("%15s", "Test3");
+	//	System.out.printf("%n%010d %n %10d", 222354 , 235654);
+		//DecimalFormat df = new DecimalFormat("#.##");
+		//System.out.println("\n" +df.format(decNum));
+	}
+		
+		
+	
+	
+}
